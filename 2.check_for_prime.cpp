138583da595369f35bd78566362f42c0131e7e98
Lakshya Saxena <42578328+lakshyasaxena@users.noreply.github.com>
2022-10-07 19:56:42 +0530

    Create 2.check_for_prime.cpp

---
+++ 2.check_for_prime.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+bool check_prime(ll num)
+{
+    if(num==1) return 0;
+    for(int i=2;i<=sqrt(num);i++)
+    {
+        if(num%i==0) return 0;
+    }
+    return 1;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false)
+    while(1)
+    {
+        cout<<"Enter your number -> ";
+        ll num;
+        cin>>num;
+        bool check=check_prime(num);
+        if(check==false)
+        {
+            cout<<"  It is not a prime\n\n";
+        }
+        else
+        {
+            cout<<"  It is a prime number\n\n";
+        }
+    }
+}
